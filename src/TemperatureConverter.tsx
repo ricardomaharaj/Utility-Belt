36ac9835607b3851f483b70bae4d95542ce84d53
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-08-07 16:21:46 -0400

    cleanup temperature logic

--- src/TemperatureConverter.tsx
+++ src/TemperatureConverter.tsx
@@ -1,71 +1,47 @@
+type Unit = 'C' | 'F' | 'K'
+
 export function TemperatureConverter() {
     document.title = 'Temperature Converter'
+
+    let tempChange = (unit: Unit) => {
+        const C = document.querySelector<HTMLInputElement>('#C')!
+        const F = document.querySelector<HTMLInputElement>('#F')!
+        const K = document.querySelector<HTMLInputElement>('#K')!
+
+        let c = parseFloat(C.value)
+        let f = parseFloat(F.value)
+        let k = parseFloat(K.value)
+
+        switch (unit) {
+            case 'C':
+                F.value = (c * (9 / 5) + 32).toString()
+                K.value = (c + 273.15).toString()
+                break
+            case 'F':
+                c = (f - 32) * (5 / 9)
+                C.value = c.toString()
+                K.value = (c + 273.15).toString()
+                break
+            case 'K':
+                c = k - 273.15
+                C.value = c.toString()
+                F.value = ((c * 9) / 5 + 32).toString()
+                break
+        }
+    }
 
     return (
         <div className='col'>
             <div className='row'>
-                <input
-                    type='number'
-                    id='C'
-                    onChange={() => {
-                        const C = document.querySelector(
-                            '#C'
-                        ) as HTMLInputElement
-                        const F = document.querySelector(
-                            '#F'
-                        ) as HTMLInputElement
-                        const K = document.querySelector(
-                            '#K'
-                        ) as HTMLInputElement
-                        let c = parseFloat(C.value)
-                        F.value = (c * (9 / 5) + 32).toString()
-                        K.value = (c + 273.15).toString()
-                    }}
-                />
+                <input type='number' id='C' onChange={() => tempChange('C')} />
                 <span>Celsius</span>
             </div>
             <div className='row'>
-                <input
-                    type='number'
-                    id='F'
-                    onChange={() => {
-                        const C = document.querySelector(
-                            '#C'
-                        ) as HTMLInputElement
-                        const F = document.querySelector(
-                            '#F'
-                        ) as HTMLInputElement
-                        const K = document.querySelector(
-                            '#K'
-                        ) as HTMLInputElement
-                        let f = parseFloat(F.value)
-                        let c = (f - 32) * (5 / 9)
-                        C.value = c.toString()
-                        K.value = (c + 273.15).toString()
-                    }}
-                />
+                <input type='number' id='F' onChange={() => tempChange('F')} />
                 <span>Fahrenheit</span>
             </div>
             <div className='row'>
-                <input
-                    type='number'
-                    id='K'
-                    onChange={() => {
-                        const C = document.querySelector(
-                            '#C'
-                        ) as HTMLInputElement
-                        const F = document.querySelector(
-                            '#F'
-                        ) as HTMLInputElement
-                        const K = document.querySelector(
-                            '#K'
-                        ) as HTMLInputElement
-                        let k = parseFloat(K.value)
-                        let c = k - 273.15
-                        C.value = c.toString()
-                        F.value = ((c * 9) / 5 + 32).toString()
-                    }}
-                />
+                <input type='number' id='K' onChange={() => tempChange('K')} />
                 <span>Kelvin</span>
             </div>
         </div>
