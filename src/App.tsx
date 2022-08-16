e5f0a27c957113bfceb634451bb83f57f6b211db
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-08-16 15:14:18 -0400

    more styling + header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { Home } from './Home'
 import { Discount } from './tools/Discount'
 import { Temperature } from './tools/Temperature'
@@ -9,13 +9,26 @@ export function App() {
     return (
         <BrowserRouter>
             <div className='container mx-auto my-2'>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/tip' element={<Tip />} />
-                    <Route path='/discount' element={<Discount />} />
-                    <Route path='/temperature' element={<Temperature />} />
-                    <Route path='/length' element={<Length />} />
-                </Routes>
+                <div className='m-2'>
+                    <Link
+                        to='/'
+                        className='row text-xl m-4 text-center justify-center'
+                    >
+                        <span>Utility Belt</span>
+                    </Link>
+                    <div className='bg-slate-800 rounded-xl p-4'>
+                        <Routes>
+                            <Route path='/' element={<Home />} />
+                            <Route path='/tip' element={<Tip />} />
+                            <Route path='/discount' element={<Discount />} />
+                            <Route
+                                path='/temperature'
+                                element={<Temperature />}
+                            />
+                            <Route path='/length' element={<Length />} />
+                        </Routes>
+                    </div>
+                </div>
             </div>
         </BrowserRouter>
     )
