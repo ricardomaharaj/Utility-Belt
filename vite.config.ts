bf855f33823d6b549e1294623c51159d4fe3ad2e
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-02-02 23:12:39 -0500

    whitespacing, use swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,43 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-    plugins: [
-        react(),
-        VitePWA({
-            manifest: {
-                short_name: 'Utility Belt',
-                name: 'Utility Belt',
-                description: 'Utility Belt',
-                icons: [
-                    {
-                        src: 'logo16.png',
-                        type: 'image/png',
-                        sizes: '16x16'
-                    },
-                    {
-                        src: 'logo32.png',
-                        type: 'image/png',
-                        sizes: '32x32'
-                    },
-                    {
-                        src: 'logo192.png',
-                        type: 'image/png',
-                        sizes: '192x192'
-                    },
-                    {
-                        src: 'logo512.png',
-                        type: 'image/png',
-                        sizes: '512x512'
-                    }
-                ],
-                start_url: '.',
-                display: 'standalone',
-                theme_color: '#000000',
-                background_color: '#000000'
-            },
-            registerType: 'autoUpdate'
-        })
-    ]
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        short_name: 'Utility Belt',
+        name: 'Utility Belt',
+        description: 'Utility Belt',
+        icons: [
+          {
+            src: 'logo16.png',
+            type: 'image/png',
+            sizes: '16x16'
+          },
+          {
+            src: 'logo32.png',
+            type: 'image/png',
+            sizes: '32x32'
+          },
+          {
+            src: 'logo192.png',
+            type: 'image/png',
+            sizes: '192x192'
+          },
+          {
+            src: 'logo512.png',
+            type: 'image/png',
+            sizes: '512x512'
+          }
+        ],
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#000000'
+      },
+      registerType: 'autoUpdate'
+    })
+  ]
 })
