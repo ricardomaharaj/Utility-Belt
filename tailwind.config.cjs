d5b4ca73cb76ebf7a64f8c03047d62243da98eca
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-04-30 22:25:01 -0400

    update configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
